19d8d886ce016fd79fdbef2e2558963d8acac611
BuildTools <unconfigured@null.spigotmc.org>
2024-07-24 22:16:03 +0800

    添加craft，优化

--- src/main/java/name/strangeworld/item/EnchantedHomeScrollItem.java
+++ src/main/java/name/strangeworld/item/EnchantedHomeScrollItem.java
@@ -1,5 +1,6 @@
 package name.strangeworld.item;
 
+import name.strangeworld.StrangeWorld;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LightningEntity;
@@ -12,6 +13,7 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.*;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -28,15 +30,25 @@ public class EnchantedHomeScrollItem extends Item {
             return TypedActionResult.success(user.getStackInHand(hand));
         }
 
-        LightningEntity lightingBolt = new LightningEntity(EntityType.LIGHTNING_BOLT,world);
-        lightingBolt.setPosition(user.getBlockPos().toCenterPos());
-        lightingBolt.animateDamage(100.0f);
-        world.spawnEntity(lightingBolt);
+        Vec3d playerPos=user.getBlockPos().toCenterPos();
+        int countOfLighting= (int) (Math.random()*5+3);
+        for(int i=0;i< countOfLighting ;i++) {
+            Vec3d spawnPos=playerPos;
+            spawnPos.add((Math.random()-0.5f)*2.0*3.0,0,(Math.random()-0.5f)*2.0*3.0);
+            StrangeWorld.LOGGER.debug(spawnPos.toString());
+            SpawnLightingBolt(world,spawnPos);
+        }
 
         ItemStack itemStack = user.getStackInHand(hand);
         itemStack.decrement(1);
 
         return TypedActionResult.success(itemStack);
+    }
+    private void SpawnLightingBolt(World world, Vec3d pos) {
+        LightningEntity lightingBolt = new LightningEntity(EntityType.LIGHTNING_BOLT,world);
+        lightingBolt.setPosition(pos);
+        //lightingBolt.animateDamage(100.0f);
+        world.spawnEntity(lightingBolt);
     }
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
